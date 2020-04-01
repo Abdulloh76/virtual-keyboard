44ec993b6d72e51cbf6740802489c1d5b3f6a7db
Abdulloh Musayev <musayevabdulloh76@gmail.com>
2020-04-01 07:20:51 +0500

    refactor: optimize classification of keys in switch

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const keyboard = document.querySelector('.keyboard');
 let keys = [
 [['`', '~'], ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], ['backspace', 'backspace']], 
 [['tab', 'tab'], ['q', 'Q'], ['w', 'W'], ['e', 'E'], ['r', 'R'], ['t', 'T'], ['y', 'Y'], ['u', 'U'], ['i', 'I'], ['o', 'O'], ['p', 'P'], ['[', '{'], [']', '}'], ['\\', '|'], ['del', 'del']],
-[['caps lock', 'caps lock'], ['a', 'A'], ['s', 'S'], ['d', 'D'], ['f', 'F'], ['g', 'G'], ['h', 'H'], ['j', 'J'], ['k', 'K'], ['l', 'L'], [';', ':'], ['\'', '"'], ['enter', 'enter']],
+[['caps-lock', 'caps lock'], ['a', 'A'], ['s', 'S'], ['d', 'D'], ['f', 'F'], ['g', 'G'], ['h', 'H'], ['j', 'J'], ['k', 'K'], ['l', 'L'], [';', ':'], ['\'', '"'], ['enter', 'enter']],
 [['shift', 'shift'], ['z', 'Z'], ['x', 'X'], ['c', 'C'], ['v', 'V'], ['b', 'B'], ['n', 'N'], ['m', 'M'], [',', '<'], ['.', '>'], ['/', '?'], ['▲', '▲'], ['shift', 'shift']],
 [['ctrl', 'ctrl'], ['win', 'win'], ['alt', 'alt'], ['space', 'space'], ['alt', 'alt'], ['◄', '◄'], ['▼', '▼'], ['►', '►'], ['ctrl', 'ctrl']]
 ];
@@ -22,33 +22,6 @@ function createKeys(isHead=0) {
         let keyRow=document.querySelectorAll('.keyboard-row');
         element.forEach(item => {
             switch(item[isHead]) {
-                case 'backspace':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key backspace">${item[isHead]}</div>`);
-                    break;
-                case 'tab':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key tab">${item[isHead]}</div>`);
-                    break;
-                case 'del':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key del">${item[isHead]}</div>`);
-                    break;
-                case 'caps lock':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key caps-lock">${item[isHead]}</div>`);
-                    break;
-                case 'enter':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key enter">${item[isHead]}</div>`);
-                    break;
-                case 'shift':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key shift">${item[isHead]}</div>`);
-                    break;
-                case 'ctrl':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key ctrl">${item[isHead]}</div>`);
-                    break;
-                case 'space':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key space">${item[isHead]}</div>`);
-                    break;
-                case 'alt':
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key alt">${item[isHead]}</div>`);
-                    break;
                 case '▲':
                     keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key upArrow">${item[isHead]}</div>`);
                     break;
@@ -62,12 +35,11 @@ function createKeys(isHead=0) {
                     keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key rightArrow">${item[isHead]}</div>`);
                     break;
                 default: 
-                    keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key">${item[isHead]}</div>`);
+                    if (item[isHead].length>2) keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key ${item[isHead]}">${item[isHead]}</div>`);
+                    else keyRow[keyRow.length-1].insertAdjacentHTML('beforeEnd', `<div class="key">${item[isHead]}</div>`);
             }
 
         });
-        
-        // element[element.length-1].style.width = '105px';
     });
 }
 createKeys(0);
